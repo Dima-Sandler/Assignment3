87983ad66f5dbfe4efe02d800168fc468eb2df1f
Dima-Sandler <dima1.san@gmail.com>
2022-04-24 20:28:24 +0300

    Update main.c

--- Adv_C_Exe3/Main.c
+++ Adv_C_Exe3/Main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-#include "Stack.h"
 
+#include "Stack.h"
 #include "Queue.h"
 
 void main()
 {
 	Queue tmp;
 	initQueue(&tmp);
-	enqueue(&tmp, 6);
-	enqueue(&tmp, 1);
-	enqueue(&tmp, 5);
-	enqueue(&tmp, 2);
-	enqueue(&tmp, 3);
-	enqueue(&tmp, 1);
-	enqueue(&tmp, 9);
-	//rotateQueue(&tmp);
-	//cutAndReplace(&tmp);
+	enqueue(&tmp, 8); // head
+	enqueue(&tmp, 11); 
+	enqueue(&tmp, 5); 
+	enqueue(&tmp, 3); 
+	enqueue(&tmp, 1); 
+	enqueue(&tmp, 4); // tail
+	rotateQueue(&tmp);
+	cutAndReplace(&tmp);
 	sortKidsFirst(&tmp);
 	destroyQueue(&tmp);
 }
